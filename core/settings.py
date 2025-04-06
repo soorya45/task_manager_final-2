5fa9b1336fcfcae375a98343e3baec27b98bb7fe
soorya45 <140391954+soorya45@users.noreply.github.com>
2025-04-06 11:39:27 +0530

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -4,7 +4,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-key'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["task-manager-final-2.onrender.com", "localhost", "127.0.0.1"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
